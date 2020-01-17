b79c44fe70416efacef0e0a88bbc6b20578f5f80
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2020-01-17 11:20:40 +0200

    Task: Refactored Model

---
+++ City/CityEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cities
+{
+    public class CityEntity : CityModel
+    {
+        [Key]
+        public int CityID { get; set; }
+
+        public virtual CountryEntity Country { get; set; }
+    }
+}
