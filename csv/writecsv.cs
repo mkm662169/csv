74730713cfbfe56e54dad2f83f78b3268a328816
mkm662169 <57430577+mkm662169@users.noreply.github.com>
2020-01-29 17:11:21 +0200

    Task: Added write csv function and test

---
+++ csv/writecsv.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Csv
+{
+    public class writecsv
+    {
+
+        public static void WriteCsvFile<T>(IList<T> myList, string fileName)
+        {
+            var query = (from s in myList select s);
+            var writePath = "c://csvfiles//" + fileName + ".csv";
+            using (var writer = new StreamWriter(writePath))
+            using (var csv = new CsvWriter(writer))
+            {
+                csv.WriteRecords(query);
+            }
+
+        }
+    }
+}
